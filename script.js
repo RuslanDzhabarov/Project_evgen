536aa61971cd2c18ab81ad5667ac95e6f165e3eb
Руслан <150541708+RuslanDzhabarov@users.noreply.github.com>
2024-04-24 23:39:33 +0300

    12

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-$	(document).ready(function(){
+$(document).ready(function(){
 	$('.header__burger').click(function(event){
 		$('.header__burger, header__menu').toggleClass('active');
 		$('.body').toggleClass('lock')
